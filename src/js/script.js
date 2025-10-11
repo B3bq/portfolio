221965933456a01bc3fb4598cc7cd756a4ae52c9
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2025-10-11 23:32:28 +0200

    navbar and added structure for screens

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+// toggle menu script
+const toggleMenuEl = document.getElementById('js-toggle-menu');
+const toggleableMenuEl = document.getElementById('js-toggleable-menu');
+const menu = document.getElementById('menu');
+let isOpen = false;
+
+toggleMenuEl?.addEventListener('click', function(){
+    toggleableMenuEl?.classList.toggle('active');
+    if(isOpen){
+        menu.src = "src/img/burger-bar.png";
+    }
+    else{
+        menu.src = "src/img/arrow.png";
+    }
+    isOpen = !isOpen; // change condition
+})
