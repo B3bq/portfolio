39aaeedef9ae3a8b66b742bc66e775b402d3e362
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2025-10-12 22:25:35 +0200

    projects imgs

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,42 @@ toggleMenuEl?.addEventListener('click', function(){
     }
     isOpen = !isOpen; // change condition
 })
+
+document.querySelectorAll('.slider').forEach(slider => {
+    const photos = slider.querySelectorAll('.photos img');
+    const nextBtn = slider.querySelector('.next');
+    const prevBtn = slider.querySelector('.prev');
+    let current = 0;
+
+    function showSlide(newIndex, direction){
+        if (newIndex === current) return;
+
+        const currentImg = photos[current];
+        const nextImg = photos[newIndex];
+
+        currentImg.classList.remove('active');
+        nextImg.classList.remove('to_left');
+        nextImg.classList.remove('to_right');
+        nextImg.classList.add('active');
+
+        if (direction == 'next'){
+            currentImg.classList.add('to_left');
+            nextImg.classList.add('active');
+        } else {
+            currentImg.classList.add('to_right');
+            nextImg.classList.add('active');
+        }
+
+        current = newIndex;
+    }
+
+    nextBtn.addEventListener('click', () => {
+        const newIndex = (current + 1) % photos.length;
+        showSlide(newIndex, 'next');
+    });
+    
+    prevBtn.addEventListener('click', () => {
+        const newIndex = (current - 1 + photos.length) % photos.length;
+        showSlide(newIndex, 'prev');
+    });
+})
